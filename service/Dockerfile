bfaf381387de9f54e92b5ecc98b3ad2cc7ab9646
vladan-stojnic <stojnic.vladan@gmail.com>
2020-12-10 23:43:55 +0100

    Combined service and webapp.
    Former-commit-id: c5d625805a7e36b43acf34bee47346c790935d73

---
+++ service/Dockerfile
@@ -0,0 +1,6 @@
+FROM tensorflow/serving
+WORKDIR /
+COPY models /models/rssc
+EXPOSE 8501
+ENV MODEL_NAME rssc
+ENV MODEL_BASE_PATH /models
---
+++ webapp/app/models.py
@@ -0,0 +1,18 @@
+class NI4OSResult():
+    def __init__(self, image=None, mimetype=None, results=None):
+        self.image = image
+        self.mimetype = mimetype
+        self.results = results
+
+
+class NI4OSData():
+    def __init__(self):
+        self.data = []
+        self.mimetype = []
+        self.results = []
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx], self.mimetype[idx], self.results[idx]
