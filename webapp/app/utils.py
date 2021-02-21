5909e1cb673aae8de730449ee02ef55b95680b0e
vladan-stojnic <stojnic.vladan@gmail.com>
2021-02-21 13:18:02 +0100

    Webapp for tagging

--- webapp/app/utils.py
+++ webapp/app/utils.py
@@ -5,7 +5,7 @@ import base64
 import json
 
 
-def parse_response(json_response):
+def parse_response(json_response, task='classification'):
     json_response = json_response.json()
     json_response = json_response['predictions']
 
@@ -13,20 +13,29 @@ def parse_response(json_response):
 
     for i, prediction in enumerate(json_response):
         preds = prediction['preds']
-        idxs = np.array(preds).argsort()[:-6:-1]
+        #idxs = np.array(preds).argsort()[:-6:-1]
 
-        top_keys = np.array(prediction['classnames'])[idxs]
-        top_values = np.array(prediction['preds'])[idxs]
-        top_values *= 100
-        top_values = top_values.astype(np.uint8)
-        top_keys = top_keys[top_values>0]
-        top_values = top_values[top_values>0]
+        keys = np.array(prediction['classnames'])
+        values = np.array(prediction['preds'])
+        values *= 100
+        values = values.astype(np.uint8)
+
+        idxs = values.argsort()[::-1]
+        top_keys = keys[idxs]
+        top_values = values[idxs]
+
+        if task.lower() == 'classification':
+            top_keys = top_keys[top_values>0]
+            top_values = top_values[top_values>0]
+        elif task.lower() == 'tagging':
+            top_keys = top_keys[top_values>50]
+            top_values = top_values[top_values>50]
 
         response.append(dict(zip(top_keys, top_values)))
 
     return response
 
-def perform_url_request(urls):
+def perform_url_request(urls, task='classification'):
     if not isinstance(urls, list):
         urls = [urls]
 
@@ -38,11 +47,16 @@ def perform_url_request(urls):
     for url in urls:
         result.append(NI4OSResult(url))
 
-    json_response = requests.post('http://localhost/url-api',
-                                  headers=headers,
-                                  data=data)
+    if task.lower() == 'classification':
+        json_response = requests.post('http://localhost/url-api',
+                                    headers=headers,
+                                    data=data)
+    elif task.lower() == 'tagging':
+        json_response = requests.post('http://localhost/multilabel-url-api',
+                                    headers=headers,
+                                    data=data)
 
-    response = parse_response(json_response)
+    response = parse_response(json_response, task)
 
     for i, out in enumerate(response):
         result[i].results = out
@@ -50,7 +64,7 @@ def perform_url_request(urls):
     return result
 
 
-def perform_upload_request(forms_data):
+def perform_upload_request(forms_data, task='classification'):
     headers = {'content-type': 'application/json'}
     req = {'signature_name': 'serving_default', 'instances': []}
 
@@ -63,11 +77,16 @@ def perform_upload_request(forms_data):
 
     data_to_send = json.dumps(req)
 
-    json_response = requests.post('http://localhost/upload-api',
-                                   headers = headers,
-                                   data=data_to_send)
+    if task.lower() == 'classification':
+        json_response = requests.post('http://localhost/upload-api',
+                                    headers = headers,
+                                    data=data_to_send)
+    elif task.lower() == 'tagging':
+        json_response = requests.post('http://localhost/multilabel-upload-api',
+                                    headers = headers,
+                                    data=data_to_send)
 
-    response = parse_response(json_response)
+    response = parse_response(json_response, task)
 
     for i, out in enumerate(response):
         result[i].results = out
