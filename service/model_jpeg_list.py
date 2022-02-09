65f90d613937dba89d86940a03cb1fb0b033abee
Vladimir Risojevic <vlado@etfbl.net>
2022-02-09 10:05:02 +0100

    Images with different sizes are allowed.
    Former-commit-id: 1e80081b4de58e018bf2c06f7104f486001a5134

--- service/model_jpeg_list.py
+++ service/model_jpeg_list.py
@@ -1,41 +1,48 @@
 import os
 import json
+import numpy as np
 import tensorflow as tf
 
 class ClassifyJPEG(tf.Module):
     def __init__(self):
         super(ClassifyJPEG, self).__init__()
-        tf.keras.backend.set_learning_phase(0)
+        #tf.keras.backend.set_learning_phase(0)
 
-        class_index = json.load(open('nwpu_class_index.json'))
+        class_index = json.load(open('mlrsnet_classes_index.json'))
         self.class_index = tf.constant(list(class_index.values()),
                                        dtype=tf.string,
-                                       shape=(1, 45))
+                                       shape=(1, 46))
 
         base_model = tf.keras.applications.resnet50.ResNet50(weights=None,
-                                                     include_top=False,
-                                                     input_shape=(256, 256, 3))
+                                                             include_top=False,
+                                                             input_shape=(256, 256, 3))
         x = base_model.output
         x = tf.keras.layers.GlobalAveragePooling2D()(x)
-        out = tf.keras.layers.Dense(45, activation='softmax')(x)
+        out = tf.keras.layers.Dense(46, activation='softmax')(x)
 
         self.clf = tf.keras.models.Model(base_model.input, out)
-        self.clf.load_weights('models/resnet50_nwpu_adam_random_crop_batch100_epochs144.h5')
+        self.clf.load_weights('models/rssc_resnet50_imagenet_MLRSNet80_ft.h5')
+
+    def __load_preprocess(self, inp):
+        img = tf.io.decode_image(inp)
+        img.set_shape((None, None, 3))
+        img = tf.image.resize(img, [256, 256])
+        means = tf.constant(np.reshape([123.68, 116.779, 103.939], (1, 1, 3)),
+                          dtype=tf.float32)
+        img = tf.math.subtract(img, means)
+
+        return img
 
     @tf.function(input_signature=[tf.TensorSpec([None,], tf.string)])
     def __call__(self, string_inp):
-        imgs_map = tf.map_fn(
-                             tf.io.decode_image,
-                             string_inp,
-                             dtype=tf.uint8
-                             #fn_output_signature=tf.RaggedTensorSpec(shape=[None, None, None, 3], dtype=tf.uint8)
-                             )
-        imgs_map.set_shape((None, None, None, 3))
-        imgs = tf.image.resize(imgs_map, [256, 256])
-        img_float = tf.cast(imgs, dtype=tf.float32) / 255.0
+        imgs = tf.map_fn(
+                         self.__load_preprocess,
+                         string_inp,
+                         fn_output_signature=tf.float32
+                        )
 
-        return {'probabilities': self.clf(img_float),
-                'classnames': tf.tile(self.class_index, [len(imgs), 1])}
+        return {'probabilities': self.clf(imgs),
+                'classnames': tf.tile(self.class_index, [tf.shape(imgs)[0], 1])}
 
 version = 1
 export_path = os.path.join('models/rssc', str(version))
--- service/multilabel_model_jpeg_list.py
+++ service/multilabel_model_jpeg_list.py
@@ -1,31 +1,39 @@
 import os
 import json
+import numpy as np
 import tensorflow as tf
 
 class ClassifyJPEG(tf.Module):
     def __init__(self):
         super(ClassifyJPEG, self).__init__()
-        tf.keras.backend.set_learning_phase(0)
+#        tf.keras.backend.set_learning_phase(0)
 
         class_index = json.load(open('mlrsnet_labels_index.json'))
         self.class_index = tf.constant(list(class_index.values()),
                                        dtype=tf.string,
                                        shape=(1, 60))
 
-        self.clf = tf.keras.models.load_model('models/rssc-resnet50-MLRSNet80_multilabel-chocolate-wave-6-2021-01-07.h5')
+        self.clf = tf.keras.models.load_model('models/rssc_resnet50_imagenet-MLRSNet80_multilabel-ft.h5')
 
+    def __load_preprocess(self, inp):
+        img = tf.io.decode_image(inp)
+        img.set_shape((None, None, 3))
+        img = tf.image.resize(img, [256, 256])
+        means = tf.constant(np.reshape([123.68, 116.779, 103.939], (1, 1, 3)),
+                            dtype=tf.float32)
+        img = tf.math.subtract(img, means)       
+
+        return img
+        
     @tf.function(input_signature=[tf.TensorSpec([None,], tf.string)])
     def __call__(self, string_inp):
-        imgs_map = tf.map_fn(
-                             tf.io.decode_image,
-                             string_inp,
-                             dtype=tf.uint8)
-        imgs_map.set_shape((None, None, None, 3))
-        imgs = tf.image.resize(imgs_map, [256, 256])
-        img_float = tf.cast(imgs, dtype=tf.float32) / 255.0
+        imgs = tf.map_fn(
+                         self.__load_preprocess,
+                         string_inp,
+                         fn_output_signature=tf.float32)
 
-        return {'probabilities': self.clf(img_float),
-                'classnames': tf.tile(self.class_index, [len(imgs), 1])}
+        return {'probabilities': self.clf(imgs),
+                'classnames': tf.tile(self.class_index, [tf.shape(imgs)[0], 1])}
 
 version = 1
 export_path = os.path.join('models/multilabel_rssc', str(version))
